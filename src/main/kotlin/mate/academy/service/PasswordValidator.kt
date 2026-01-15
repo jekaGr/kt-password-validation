a79f5fd6f0e7e4e21e00ac74de2775fcf4f0b424
jekag <jekagrusha@gmail.com>
2026-01-15 20:51:51 +0200

    fix after review

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -3,8 +3,11 @@ package mate.academy.service
 import mate.academy.exception.PasswordValidationException
 
 class PasswordValidator {
+    companion object {
+        private const val MIN_PASSWORD_LENGTH = 10
+    }
     fun validate(password: String, repeatPassword: String) {
-        if (password != repeatPassword || password.length < 10) {
+        if (password != repeatPassword || password.length < MIN_PASSWORD_LENGTH) {
             throw PasswordValidationException("Wrong passwords")
         }
     }
